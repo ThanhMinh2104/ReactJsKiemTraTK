eb4ee073c986d285e7cad786ea6dec925aace85c
Nguyen Ho Thanh Minh <nghothminz@gmail.com>
2025-04-08 20:06:19 +0700

    5.Click Edit -> Modal + get API

--- kiemtratk/src/components/Content.jsx
+++ kiemtratk/src/components/Content.jsx
@@ -1,51 +1,68 @@
-import { useState } from "react"
-import { useEffect } from "react"
-import { data } from "react-router-dom"
+import { useState, useEffect } from "react";
 
 export default function Content() {
-    const [overviews, setOverView] = useState([])
-    const [datas, setData] = useState([])
+    const [overviews, setOverView] = useState([]);
+    const [datas, setData] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [editData, setEditData] = useState(null);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch("http://localhost:3000/overview")
             .then(resp => resp.json())
-            .then(datas => 
-                setOverView(datas)
-            )
-    }, [])
+            .then(datas => setOverView(datas));
+    }, []);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch("http://localhost:3000/table")
             .then(resp => resp.json())
-            .then(data => 
-                setData(data)
+            .then(data => setData(data));
+    }, []);
+
+    const handleEditClick = (data) => {
+        setEditData(data);
+        setShowModal(true);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEditData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    const handleSave = () => {
+        setData(prev =>
+            prev.map(item =>
+                item.id === editData.id ? editData : item
             )
-    }, [])
-    
+        );
+        setShowModal(false);
+    };
+
     return (
         <div className="content">
+            {/* Overview */}
             <div className="overview">
                 <h1>Overview</h1>
                 <ul>
-                    {
-                        overviews.map((overview) => (
-                            <li key={overview.id} className="overview-item">
-                                <div className="overview-item_data">
-                                    <h4 className="overview_item-title">{overview.title}</h4>
-                                    <h1 className="overview_item-cost">${overview.value}</h1>
-                                    <p className="overview_item-perchange">{overview.changePercent} period of change</p>
-                                </div>
-                                <div
-                                    className="overview-item_icon"
-                                    dangerouslySetInnerHTML={{ __html: overview.icon }}
-                                ></div>
-
-                            </li>
-                        ))
-                    }
+                    {overviews.map((overview) => (
+                        <li key={overview.id} className="overview-item">
+                            <div className="overview-item_data">
+                                <h4 className="overview_item-title">{overview.title}</h4>
+                                <h1 className="overview_item-cost">${overview.value}</h1>
+                                <p className="overview_item-perchange">{overview.changePercent} period of change</p>
+                            </div>
+                            <div
+                                className="overview-item_icon"
+                                dangerouslySetInnerHTML={{ __html: overview.icon }}
+                            ></div>
+                        </li>
+                    ))}
                 </ul>
             </div>
 
+            {/* Detail Report */}
             <div className="detailReport">
                 <div className="detailReport_header">
                     <span>Detail Report</span>
@@ -59,7 +76,7 @@ export default function Content() {
                     <table>
                         <thead>
                             <tr>
-                                <th className="customer_check"><input type="checkbox" name="" id="" /></th>
+                                <th className="customer_check"><input type="checkbox" /></th>
                                 <th>CUSTOMER NAME</th>
                                 <th>COMPANY</th>
                                 <th>ORDER VALUE</th>
@@ -70,42 +87,66 @@ export default function Content() {
                         </thead>
 
                         <tbody>
-                            {
-                                datas.map((data) => (
-                                    <tr key={data.id}>
-                                        <td className="customer_check"><input type="checkbox" name="" id="" /></td>
-                                        <td className="customer_name">
-                                            <img src={data.img} className="customer_avatar" alt="" />
-                                            {data.name}
-                                        </td>
-                                        <td>{data.company}</td>
-                                        <td>{data.orderValue}</td>
-                                        <td>{data.orderDate}</td>
-                                        <td className="row_status">{data.status}</td>
-                                        <td className="customer_edit"><i className="fa-solid fa-pen"></i></td>
-                                    </tr>
-                                ))
-                            }
+                            {datas.map((data) => (
+                                <tr key={data.id}>
+                                    <td className="customer_check"><input type="checkbox" /></td>
+                                    <td className="customer_name">
+                                        <img src={data.img} className="customer_avatar" alt="" />
+                                        {data.name}
+                                    </td>
+                                    <td>{data.company}</td>
+                                    <td>{data.orderValue}</td>
+                                    <td>{data.orderDate}</td>
+                                    <td className="row_status">{data.status}</td>
+                                    <td className="customer_edit">
+                                        <i className="fa-solid fa-pen" onClick={() => handleEditClick(data)}></i>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
-
                     </table>
                 </div>
 
                 <div className="detailReport_footer">
                     <div className="detailReport_result">63 results</div>
                     <ul className="detailReport_changPage">
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
+                        {[1, 2, 3, 4, 5].map((num) => <li key={num}>{num}</li>)}
                     </ul>
                 </div>
             </div>
+
+            {/* Modal */}
+            {showModal && editData && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <h2>Edit Customer</h2>
+                        <label>
+                            Name:
+                            <input name="name" value={editData.name} onChange={handleChange} />
+                        </label>
+                        <label>
+                            Company:
+                            <input name="company" value={editData.company} onChange={handleChange} />
+                        </label>
+                        <label>
+                            Order Value:
+                            <input name="orderValue" value={editData.orderValue} onChange={handleChange} />
+                        </label>
+                        <label>
+                            Order Date:
+                            <input name="orderDate" value={editData.orderDate} onChange={handleChange} />
+                        </label>
+                        <label>
+                            Status:
+                            <input name="status" value={editData.status} onChange={handleChange} />
+                        </label>
+                        <div className="modal-buttons">
+                            <button onClick={handleSave}>Save</button>
+                            <button onClick={() => setShowModal(false)}>Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
